db21da28a653c9ff09dce0c85b4f6a68e6648106
Hubert Łyczek <hlyczek@gmail.com>
2026-03-11 11:46:06 +0100

    @RequestBody - support for Lists

--- src/main/java/online/itlab/springframework/validation/errors/standard/factory/tools/ReflectionTools.java
+++ src/main/java/online/itlab/springframework/validation/errors/standard/factory/tools/ReflectionTools.java
@@ -3,33 +3,82 @@ package online.itlab.springframework.validation.errors.standard.factory.tools;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.Map;
 
 // NOTE: Spring provides itw own implementation of reflection tools
 //       import org.springframework.util.ReflectionUtils;
 // TODO evaluate Spring implementation and it's usage
 public class ReflectionTools implements IReflectionTools {
 
-    public String toJsonPath(final Class<?> rootClass, final String javaPath) {
-        final String[] parts = javaPath.split("\\.");
+    /**
+     *
+     * @param rootClass
+     * @param indexedJavaPath Dot separated path composed of java field names.
+     *                        List: javaFieldName[indexPart], eg: loans[0].guarantors[1].firstName
+     * @return
+     */
+    public String toJsonPath(final Class<?> rootClass, final String indexedJavaPath) {
+        final String[] pathSegments = indexedJavaPath.split("\\.");
         final StringBuilder result = new StringBuilder();
 
         Class<?> current = rootClass;
-        for (int i = 0; i < parts.length; i++) {
-            final String part = parts[i];
+        for (int i = 0; i < pathSegments.length; i++) {
+            final String pathSegment = pathSegments[i];
+            final PathSegment decomposedPathSegment = decompose(pathSegment);
 
-            final Field field = findField(current, part);
+            final Field field = findField(current, decomposedPathSegment.javaFieldName);
             final String jsonName = getJsonName(field);
 
             if (i > 0) {
                 result.append(".");
             }
             result.append(jsonName);
+            result.append(decomposedPathSegment.indexPart);
 
-            current = field.getType();
+            // if a field is a collection we need to take the class of the actual element
+            // TODO getNextSegmentClass ignores decomposedPathSegment.indexPart
+            //      If a field is a collection and indexPart is empty String this is IllegalStateException
+            //      Also if field is not a collection and indexPart is not empty this is IllegalStateException
+            //      This should never happen as long as Jakarta Validation works correctly
+            //      Consider handling this situation.
+            current = getNextSegmentClass(field);
         }
 
         return result.toString();
+    }
+
+    /**
+     * Returns a class to follow the indexed java path.
+     * Result:
+     * - actual class -> actual class
+     * - collection -> parametrized class
+     */
+    private Class<?> getNextSegmentClass(final Field field) {
+        final boolean isCollection = Collection.class.isAssignableFrom(field.getType());
+        if (isCollection) {
+            Type genericType = field.getGenericType();
+            if (genericType instanceof ParameterizedType parameterizedType) {
+                Type argType = parameterizedType.getActualTypeArguments()[0];
+                if (argType instanceof Class<?>) {
+                    return (Class<?>) argType;
+                }
+
+                // TODO List of lists - still needs to be supported
+                if (argType instanceof ParameterizedType pt) {
+                    return (Class<?>) pt.getRawType();
+                }
+            }
+        }
+        final boolean isMap = Map.class.isAssignableFrom(field.getType());
+        if (isMap) {
+            // TODO temporal destructive behavior - replace with proper handling
+            throw new UnsupportedOperationException("Cannot follow Maps");
+        }
+        return field.getType();
     }
 
     public Field findField(final Class<?> type, final String javaName) {
@@ -41,10 +90,26 @@ public class ReflectionTools implements IReflectionTools {
             ));
     }
 
+    private PathSegment decompose(final String pathSegment) {
+        int idx = pathSegment.indexOf('[');
+        if (idx == -1) {
+            return new PathSegment(pathSegment, "");
+        }
+        return new PathSegment(
+            pathSegment.substring(0, idx),
+            pathSegment.substring(idx)
+        );
+    }
+
     private String getJsonName(final Field field) {
         final JsonProperty jsonProperty = field.getAnnotation(JsonProperty.class);
         return jsonProperty != null
             ? jsonProperty.value()
             : field.getName();
     }
+
+    private record PathSegment(
+        String javaFieldName,
+        String indexPart
+    ) {}
 }
--- src/test/groovy/online/itlab/springframework/validation/errors/standard/factory/tools/ReflectionToolsSpec.groovy
+++ src/test/groovy/online/itlab/springframework/validation/errors/standard/factory/tools/ReflectionToolsSpec.groovy
@@ -2,6 +2,7 @@ package online.itlab.springframework.validation.errors.standard.factory.tools
 
 import com.fasterxml.jackson.annotation.JsonProperty
 import groovy.transform.KnownImmutable
+import lombok.Builder
 import spock.lang.Specification
 
 /**
@@ -21,11 +22,14 @@ class ReflectionToolsSpec extends Specification {
         then:
             jsonPath == expectedJsonPath
         where:
-            javaPath           | clazz           || expectedJsonPath
-            'person.firstName' | Employee        || javaPath
-            'position'         | Employee        || javaPath
-            'person.firstName' | EmployeeRenamed || 'pe.fn'
-            'position'         | EmployeeRenamed || 'po'
+            javaPath                           | clazz           || expectedJsonPath
+            'person.firstName'                 | Employee        || javaPath
+            'position'                         | Employee        || javaPath
+            'person.firstName'                 | EmployeeRenamed || 'pe.fn'
+            'position'                         | EmployeeRenamed || 'po'
+            // collections part
+            'loans[1].guarantors[3].firstName' | Finances        || 'loans[1].guarantors[3].firstName'
+            'loans[1].guarantors[3].firstName' | FinancesRenamed || 'l[1].g[3].fn'
     }
 
     def 'throws exception when there is no java path'() {
@@ -36,8 +40,8 @@ class ReflectionToolsSpec extends Specification {
             exception.message == expectedExceptionMessage
         where:
             javaPath           | clazz           || expectedExceptionMessage
-            'person.xxx'       | Employee || "Field 'xxx' not found in ${Person.name}"
-            'aaa.bbb'          | Employee || "Field 'aaa' not found in ${Employee.name}"
+            'person.xxx'       | Employee        || "Field 'xxx' not found in ${Person.name}"
+            'aaa.bbb'          | Employee        || "Field 'aaa' not found in ${Employee.name}"
     }
 
     def 'correctly retrieves Field from record and class'() {
@@ -85,4 +89,40 @@ class ReflectionToolsSpec extends Specification {
         String lastName;
         Integer height;
     }
+
+
+    record CreditCard(
+        BigDecimal availableBalance,
+        Map<String, BigDecimal> spendingByCategory
+    ){}
+
+    record Loan(
+        BigDecimal initialAmount,
+        BigDecimal currentAmount,
+        List<Person> guarantors
+    ){}
+
+    record Finances(
+        List<CreditCard> creditCards,
+        List<Loan> loans
+    ){}
+
+    @Builder
+    record CreditCardRenamed(
+        @JsonProperty("ab") BigDecimal availableBalance,
+        @JsonProperty("sbc") Map<String, BigDecimal> spendingByCategory
+    ){}
+
+    @Builder
+    record LoanRenamed(
+        @JsonProperty("ia") BigDecimal initialAmount,
+        @JsonProperty("ca") BigDecimal currentAmount,
+        @JsonProperty("g") List<PersonRenamed> guarantors
+    ){}
+
+    @Builder
+    record FinancesRenamed(
+        @JsonProperty("cc") List<CreditCardRenamed> creditCards,
+        @JsonProperty("l") List<LoanRenamed> loans
+    ){}
 }
